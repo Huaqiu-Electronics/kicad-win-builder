2a80cf4ad3b57ec22085b36288fa99481950fef8
Marek Roszko <mark.roszko@gmail.com>
2021-02-06 10:06:29 -0500

    Undo fresh flag again

--- build.ps1
+++ build.ps1
@@ -550,7 +550,7 @@ function Build-Kicad {
         [ValidateSet('Release', 'Debug')]
         [string]$buildType = 'Release',
         [Parameter(Mandatory=$False)]
-        [bool]$fresh = $True
+        [bool]$fresh = $False
     )
 
     $buildName = Get-Build-Name -Arch $arch -BuildType $buildType
