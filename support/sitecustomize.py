e0680cd00d86ed246c4f862dcf8289ba46aa043c
Marek Roszko <mark.roszko@gmail.com>
2021-11-27 22:32:41 -0500

    Use win32 api to get kicad version
    Fixes https://gitlab.com/kicad/packaging/kicad-win-builder/-/issues/138

--- support/sitecustomize.py
+++ support/sitecustomize.py
@@ -23,15 +23,55 @@
 import site
 import os
 import pathlib
-import re
 import sys
+import array
+from ctypes import *
+
+def get_file_version(filename):
+    """
+    Extract information from a file.
+    Original from https://stackoverflow.com/questions/42604493/verqueryvaluew-issue-python-3
+    """
+    # Get size needed for buffer (0 if no info)
+    size = windll.version.GetFileVersionInfoSizeW(filename, None)
+    # If no info in file -> empty string
+    if not size:
+        return 'Failed'
+
+    # Create buffer
+    file_version_buffer = create_string_buffer(size)
+
+    # Load file informations into buffer res
+    windll.version.GetFileVersionInfoW(filename, None, size, file_version_buffer)
+
+    # r is the void pointer into the buffer
+    r = c_void_p(0)
+    # l is the length read at the buffer pointer
+    l = c_uint(0)
+
+    # Look for codepages
+    success = windll.version.VerQueryValueW(file_version_buffer, u'\\VarFileInfo\\Translation', byref(r), byref(l))
+
+    # If no codepage -> empty string
+    if not l.value:
+        return ''
+    
+    # Take the first codepage (what else ?)
+    codepages = array.array('H', string_at(r.value, l.value))
+    codepage = tuple(codepages[:2].tolist())
+
+    # Extract information
+    windll.version.VerQueryValueW(file_version_buffer, ('\\StringFileInfo\\%04x%04x\\' + 'ProductVersion') % codepage, byref(r), byref(l))
+
+    #-1 length because it includes null
+    return wstring_at(r.value, l.value-1)
 
 sys.path = []
 
-directory = os.path.dirname(os.path.realpath(__file__))
-site.addsitedir(os.path.join(directory, '..\\..\\DLLs'))
-site.addsitedir(os.path.join(directory, '..\\..\\Lib'))
-site.addsitedir(os.path.join(directory, '..\\..\\Lib\\site-packages'))
+current_directory = os.path.dirname(os.path.realpath(__file__))
+site.addsitedir(os.path.join(current_directory, '..\\..\\DLLs'))
+site.addsitedir(os.path.join(current_directory, '..\\..\\Lib'))
+site.addsitedir(os.path.join(current_directory, '..\\..\\Lib\\site-packages'))
 
 # Python is a language designed by committee 
 # with absolutely zero consistency as a result and half baked everything
@@ -41,26 +81,30 @@ site.addsitedir(os.path.join(directory, '..\\..\\Lib\\site-packages'))
 # Uninstall however does not do the logical thing
 # It is driven by sys.path which is appended to by site.addsitedir
 
-self_dir_path = str(pathlib.Path(__file__).parent.resolve())
-# extract the version number
-m = re.search(r'\\(\d.+)\\bin', self_dir_path)
-version = m.group(1)
+kicad_path = os.path.normpath(os.path.join(current_directory, '..\\..\\kicad.exe'))
 
-# sysconfig override
-user_base = os.path.expanduser(f'~\\Documents\\KiCad\\{version}\\3rdparty\\')
-os.environ["PYTHONUSERBASE"] = user_base
+kicad_version = ""
+try:
+    kicad_version = get_file_version(kicad_path)
+except:
+    pass
 
-# distutils overrides
-# Even worse, sysconfig has lots of hardcoded logic to 
-# always append "Python{MAJOR}.{MINOR}" to the PYTHONUSERBASE...which we can spend a hundred lines overriding
-# or reproduce it for the `distutils` override
-python_ver_nodot = sys.winver.replace('.', '')
-user_site = os.path.expanduser(f'~\\Documents\\KiCad\\{version}\\3rdparty\\Python{python_ver_nodot}\\site-packages')
-# Now override the site params which drive distutils
-site.PREFIXES = [ self_dir_path ]
-site.USER_BASE = user_base
-site.USER_SITE = user_site
+if kicad_version:
+    # sysconfig override
+    user_base = os.path.expanduser(f'~\\Documents\\KiCad\\{kicad_version}\\3rdparty\\')
+    os.environ["PYTHONUSERBASE"] = user_base
 
-# sys.paths override
-# Because pip ignores all of the above and uses sys.paths for finding packages to remove
-site.addsitedir(user_site)
+    # distutils overrides
+    # Even worse, sysconfig has lots of hardcoded logic to 
+    # always append "Python{MAJOR}.{MINOR}" to the PYTHONUSERBASE...which we can spend a hundred lines overriding
+    # or reproduce it for the `distutils` override
+    python_ver_nodot = sys.winver.replace('.', '')
+    user_site = os.path.expanduser(f'~\\Documents\\KiCad\\{kicad_version}\\3rdparty\\Python{python_ver_nodot}\\site-packages')
+    # Now override the site params which drive distutils
+    site.PREFIXES = [ current_directory ]
+    site.USER_BASE = user_base
+    site.USER_SITE = user_site
+
+    # sys.paths override
+    # Because pip ignores all of the above and uses sys.paths for finding packages to remove
+    site.addsitedir(user_site)
