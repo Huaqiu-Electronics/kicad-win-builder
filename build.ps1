19506ad560b7aefdcefca33b3027a8bf719d3940
Ethan Chien <liangtie.qian@gmail.com>
2025-01-23 22:52:51 +0800

    feat: Update package version to 8.99 in kicad-nightly.json and change default build config to kicad-nightly

--- build.ps1
+++ build.ps1
@@ -100,7 +100,7 @@ param(
     [Parameter(Mandatory=$False, ParameterSetName="package")]
     [Parameter(Mandatory=$False, ParameterSetName="preparepackage")]
     # NOTE Change to your build config
-    [string]$BuildConfigName = 'kicad-hq',
+    [string]$BuildConfigName = 'kicad-nightly',
 
     [Parameter(Mandatory=$False, ParameterSetName="build")]
     [Parameter(Mandatory=$False, ParameterSetName="package")]
